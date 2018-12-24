4386edf71d06d482b23ec84a804da72d8a7853df
Jordan Link <jordanlink@Jordans-MacBook-Pro.local>
2018-12-23 22:08:25 -0500

    save

--- stocks.py
+++ stocks.py
@@ -1,5 +1,7 @@
 import csv
 import time
+import requests
+import json
 
 
 api_rows = []
@@ -7,7 +9,14 @@ with open('30DayByMinute.csv') as file:
     csv_reader = csv.reader(file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        api_rows.append(row)
+        api_rows.append(str(row).strip("['").strip("']"))
         line_count += 1
 
-    print(api_rows[5)
+    for url in api_rows:
+        data = requests.get(url).json()
+        time.sleep(.2)
+        print()
+
+
+
+tickers = [‘AAPL','ABT','ABX','AES','AIG','AKS','AMAT','AMD','ATVI','AUY','AVP','AXP','BA','BAC','BB','BBD','BILI','BK','BRK.B','BSX','BVN','C','CAG','CAT','CE','CHK','COP','CSCO','CTL','CVS','CVX','DIS','DNR','DVN','DWDP','ECA','ERIC','ESV','F','FB','FCX','GE','GFI','GG','GM','GNW','GS','HAL','HD','HMNY','HPE','HPQ','IBM','INFY','INTC','JNJ','JPM','KEY','KGC','KO','LYG','MCD','MGM','MMM','MO','MRIN','MRK','MRO','MS','MSFT','MU','NBR','NEM','NKE','NLST','NLY','NOK','NVDA','ORCL','PBR','PFE','PG','PM','PPL','QCOM','RAD','RF','RIG','S','SLB','SMCI','SNAP','SQ','SWN','SYMC','T','TRV','TSM','TWTR','TXN','UNH','USB','UTX','V','VALE','VZ','WBA','WFC','WFT','WMB','WMT','WY','XOM','ZNGA’]
