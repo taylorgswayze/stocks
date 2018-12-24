4a64094edfc7256078216dc329292df67173be23
Swayze <taylor.swayze@wwwinc.com>
2018-12-23 22:45:12 -0500

    'save'

--- stocks.py
+++ stocks.py
@@ -1,7 +1,18 @@
 import csv
 import time
+from datetime import date, timedelta
 import requests
 import json
+
+#date = time.strftime('%Y%m%d')
+yesterday = date.today() - timedelta(3)
+date2 = yesterday.strftime('%Y%m%d')
+print(date)
+
+tickers = ['AAPL', 'ABT', 'ABX', 'AES', 'AIG', 'AKS', 'AMAT', 'AMD', 'ATVI', 'AUY', 'AVP', 'AXP', 'BA', 'BAC', 'BB', 'BBD', 'BILI', 'BK', 'BSX', 'BVN', 'C', 'CAG', 'CAT', 'CE', 'CHK', 'COP', 'CSCO', 'CTL', 'CVS', 'CVX', 'DIS', 'DNR', 'DVN', 'DWDP', 'ECA', 'ERIC', 'ESV', 'F', 'FB', 'FCX', 'GE', 'GFI', 'GG', 'GM', 'GNW', 'GS', 'HAL', 'HD', 'HMNY', 'HPE', 'HPQ', 'IBM', 'INFY', 'INTC', 'JNJ', 'JPM', 'KEY', 'KGC', 'KO', 'LYG', 'MCD', 'MGM', 'MMM', 'MO', 'MRIN', 'MRK', 'MRO', 'MS', 'MSFT', 'MU', 'NBR', 'NEM', 'NKE', 'NLST', 'NLY', 'NOK', 'NVDA', 'ORCL', 'PBR', 'PFE', 'PG', 'PM', 'PPL', 'QCOM', 'RAD', 'RF', 'RIG', 'S', 'SLB', 'SMCI', 'SNAP', 'SQ', 'SWN', 'SYMC', 'T', 'TRV', 'TSM', 'TWTR', 'TXN', 'UNH', 'USB', 'UTX', 'V', 'VALE', 'VZ', 'WBA', 'WFC', 'WFT', 'WMB', 'WMT', 'WY', 'XOM', 'ZNGA']
+for i in tickers:
+    url = 'https://api.iextrading.com/1.0/stock/' + i + '/chart/date/' + date2
+    print(url)
 
 
 api_rows = []
@@ -18,5 +29,3 @@ with open('30DayByMinute.csv') as file:
         print()
 
 
-
-tickers = [‘AAPL','ABT','ABX','AES','AIG','AKS','AMAT','AMD','ATVI','AUY','AVP','AXP','BA','BAC','BB','BBD','BILI','BK','BRK.B','BSX','BVN','C','CAG','CAT','CE','CHK','COP','CSCO','CTL','CVS','CVX','DIS','DNR','DVN','DWDP','ECA','ERIC','ESV','F','FB','FCX','GE','GFI','GG','GM','GNW','GS','HAL','HD','HMNY','HPE','HPQ','IBM','INFY','INTC','JNJ','JPM','KEY','KGC','KO','LYG','MCD','MGM','MMM','MO','MRIN','MRK','MRO','MS','MSFT','MU','NBR','NEM','NKE','NLST','NLY','NOK','NVDA','ORCL','PBR','PFE','PG','PM','PPL','QCOM','RAD','RF','RIG','S','SLB','SMCI','SNAP','SQ','SWN','SYMC','T','TRV','TSM','TWTR','TXN','UNH','USB','UTX','V','VALE','VZ','WBA','WFC','WFT','WMB','WMT','WY','XOM','ZNGA’]
